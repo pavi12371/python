375b8e07c274dfc0e295d059326262fa43020dcc
pavi12371 <peetlapavithra52@gmail.com>
2025-04-04 03:22:03 -0700

    Add files via upload

---
+++ exp1.2.py
@@ -0,0 +1,8 @@
+a=int(input("enter the first value:"))
+b=int(input("enter the second value:"))
+print("the addition is:",a+b)
+print("the difference is:",a-b)
+print("the multiplication is:",a*b)
+print("the division is :",a/b)
+print("the modulu is :",a%b)
+print("the floor division is:",a//b)
